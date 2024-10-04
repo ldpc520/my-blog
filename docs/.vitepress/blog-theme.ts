12f34ffd52564ffa56b32a96b98af79c88e51f5f
ldpc520 <62380221+ldpc520@users.noreply.github.com>
2024-10-04 17:54:18 +0800

    Update blog-theme.ts

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -68,37 +68,37 @@ const blogTheme = getThemeConfig({
 
   // 公告
   popover: {
-    title: '公告',
-    body: [
-      { type: 'text', content: '👇公众号👇---👇 微信 👇' },
-      {
-        type: 'image',
-        src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
-      },
-      {
-        type: 'text',
-        content: '欢迎大家加群&私信交流'
-      },
-      {
-        type: 'text',
-        content: '文章首/文尾有群二维码',
-        style: 'padding-top:0'
-      },
-      {
-        type: 'button',
-        content: '作者博客',
-        link: 'https://sugarat.top'
-      },
-      {
-        type: 'button',
-        content: '加群交流',
-        props: {
-          type: 'success'
-        },
-        link: 'https://theme.sugarat.top/group.html',
-      }
-    ],
-    duration: 0
+    // title: '公告',
+    // body: [
+    //   { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+    //   {
+    //     type: 'image',
+    //     src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
+    //   },
+    //   {
+    //     type: 'text',
+    //     content: '欢迎大家加群&私信交流'
+    //   },
+    //   {
+    //     type: 'text',
+    //     content: '文章首/文尾有群二维码',
+    //     style: 'padding-top:0'
+    //   },
+    //   {
+    //     type: 'button',
+    //     content: '作者博客',
+    //     link: 'https://sugarat.top'
+    //   },
+    //   {
+    //     type: 'button',
+    //     content: '加群交流',
+    //     props: {
+    //       type: 'success'
+    //     },
+    //     link: 'https://theme.sugarat.top/group.html',
+    //   }
+    // ],
+    // duration: 0
   },
 })
 
