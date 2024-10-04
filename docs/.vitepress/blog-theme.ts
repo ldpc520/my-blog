913845b6747e6727b2aaea0c37eca729b6801048
ldpc520 <62380221+ldpc520@users.noreply.github.com>
2024-10-04 17:34:45 +0800

    Update blog-theme.ts

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -51,7 +51,7 @@ const blogTheme = getThemeConfig({
   // 友链
   friend: [
     {
-      nickname: '炒花生送白粥',
+      nickname: '粥里有勺糖',
       des: '你的指尖用于改变世界的力量',
       avatar:
         'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
